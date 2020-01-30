a6cfe25aff0980f7213d5c287713fca089f922a2
1im <mcgahajr@ornl.gov>
2020-01-30 16:33:43 -0500

    Update type hinting and default values

--- pydnd/creature.py
+++ pydnd/creature.py
@@ -3,18 +3,19 @@ import typing
 
 class Creature:
     def __init__(self):
-        self.name = None
-        self.size = None
-        self.type = None
-        self.alignment = None
-        self.armor_class = None
-        self.hitpoints = None
-        self.speed = None
-        self.attributes = None
-        self.saving_throws = None
-        self.skills = None
-        self.damage_resistances = None
-        self.damage_vulnerabilities = None
-        self.damage_immunities = None
-        self.senses = None
-        self.languages = None
+        self.name: str = ''
+        self.size: str = ''
+        self.type: str = ''
+        self.alignment: typing.Tuple[str, str] = ('', '')
+        self.armor_class: int = 10
+        self.hitpoints: int = 0
+        self.speed: int = 0
+        self.attributes: typing.Any = None
+        # Attributes will eventually have custom handler class
+        self.saving_throws: typing.List[str] = list()
+        self.skills: typing.List[str] = list()
+        self.damage_resistances: typing.List[str] = list()
+        self.damage_vulnerabilities: typing.List[str] = list()
+        self.damage_immunities: typing.List[str] = list()
+        self.senses: typing.List[str] = list()
+        self.languages: typing.List[str] = list()
