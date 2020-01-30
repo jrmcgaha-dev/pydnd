79870e3c707af8b04cf88ddcf716d778f6b12f58
1im <mcgahajr@ornl.gov>
2020-01-30 15:54:52 -0500

    Added basic existence testing for class attributes

--- test/test_creature.py
+++ test/test_creature.py
@@ -7,3 +7,22 @@ def test_imports():
 
 def test_class_existence():
     assert callable(creature.Creature)
+
+
+def test_class_attribute_existence():
+    empty_creature = creature.Creature()
+    assert hasattr(empty_creature, 'name')
+    assert hasattr(empty_creature, 'size')
+    assert hasattr(empty_creature, 'type')
+    assert hasattr(empty_creature, 'alignment')
+    assert hasattr(empty_creature, 'armor_class')
+    assert hasattr(empty_creature, 'hitpoints')
+    assert hasattr(empty_creature, 'speed')
+    assert hasattr(empty_creature, 'attributes')
+    assert hasattr(empty_creature, 'saving_throws')
+    assert hasattr(empty_creature, 'skills')
+    assert hasattr(empty_creature, 'damage_resistances')
+    assert hasattr(empty_creature, 'damage_vulnerabilities')
+    assert hasattr(empty_creature, 'damage_immunities')
+    assert hasattr(empty_creature, 'senses')
+    assert hasattr(empty_creature, 'languages')
