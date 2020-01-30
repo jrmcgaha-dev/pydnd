a2af1f6a1e4750860272adfa544ca6ba05bc28b1
1im <mcgahajr@ornl.gov>
2020-01-30 16:00:44 -0500

    Create class attribute initialization

--- pydnd/creature.py
+++ pydnd/creature.py
@@ -3,4 +3,18 @@ import typing
 
 class Creature:
     def __init__(self):
-        pass
+        self.name = None
+        self.size = None
+        self.type = None
+        self.alignment = None
+        self.armor_class = None
+        self.hitpoints = None
+        self.speed = None
+        self.attributes = None
+        self.saving_throws = None
+        self.skills = None
+        self.damage_resistances = None
+        self.damage_vulnerabilities = None
+        self.damage_immunities = None
+        self.senses = None
+        self.languages = None
