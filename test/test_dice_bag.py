4ab92686985f7a158d89f53eae234130bb6edfc8
1im <mcgahajr@ornl.gov>
2020-02-05 12:08:06 -0500

    Create test for intended functionality of _roll_d method

--- test/test_dice_bag.py
+++ test/test_dice_bag.py
@@ -4,3 +4,11 @@ from pydnd import dice_bag
 def test_imports():
     assert hasattr(dice_bag, 'random')
     assert hasattr(dice_bag, 're')
+
+
+def test_roll_d():
+    assert hasattr(dice_bag, '_roll_d')
+    assert callable(dice_bag._roll_d)
+    assert dice_bag._roll_d(1) == 1
+    assert 1 <= dice_bag._roll_d(10) <= 10
+    assert 1 <= dice_bag._roll_d(1000) <= 1000
