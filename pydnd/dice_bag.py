6d0792161ad1bfa7d620ef7df3cddd6b4b746a10
1im <mcgahajr@ornl.gov>
2020-02-05 12:25:47 -0500

    Add logger and _roll_d method

--- pydnd/dice_bag.py
+++ pydnd/dice_bag.py
@@ -1,2 +1,20 @@
+import logging
 import random
 import re
+
+
+logging.basicConfig(level=logging.DEBUG)
+# Remove above before merge
+_log = logging.getLogger(__name__)
+
+
+def _roll_d(m_val: int) -> int:
+    _log.debug("m_val == %r", m_val)
+    if not isinstance(m_val, int):
+        _log.warning("_roll_val only accepts int values")
+        return 0
+    if m_val < 1:
+        _log.warning("_roll_val only accepts values >= 1")
+        return 0
+    _log.debug("returning random.randint(1, %r)", m_val)
+    return random.randint(1, m_val)
