5525c5a3c27aab010562beae7c254a889ef47c82
1im <mcgahajr@ornl.gov>
2020-02-06 10:37:22 -0500

    Add RANDINT_FUNCTION typing constant and implement default randint for Roller

--- pydnd/dice_bag.py
+++ pydnd/dice_bag.py
@@ -5,13 +5,15 @@ based upon a limited set of the commands available on roll20.net
 import logging
 import random
 import re
+import typing
 
 
 logging.basicConfig(level=logging.DEBUG)
 # Remove above before merge
 _log = logging.getLogger(__name__)
+RANDINT_FUNCTION = typing.Callable[[int, int], int]
 
 
 class Roller:
-    def __init__(self):
-        pass
+    def __init__(self, randint_function: RANDINT_FUNCTION = random.randint):
+        self._randint = randint_function
