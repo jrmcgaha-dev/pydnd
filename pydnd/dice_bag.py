1eff21f69809adb0fd5459a760b3a6d9bb2b69a8
1im <mcgahajr@ornl.gov>
2020-02-07 13:10:29 -0500

    Add debug messages to _parse_command and _dice_pool

--- pydnd/dice_bag.py
+++ pydnd/dice_bag.py
@@ -28,10 +28,15 @@ class Roller:
 
     @staticmethod
     def _parse_command(command: str) -> typing.Tuple:
+        _log.debug("command == %r", command)
         command = command + ' '
         _command, _message = command.split(' ', 1)
+        _log.debug("_command == %r", _command)
+        _log.debug("_message == %r", _message)
         _command = str.replace(_command, '-', '+-')
+        _log.debug("- to +-: _command == %r", _command)
         _actions = _command.split('+')
+        _log.debug("_actions == %r", _actions)
         _parsed = (
             int(item)
             if re.fullmatch('\\d+', item)
@@ -45,14 +50,18 @@ class Roller:
                    sides: int,
                    reroll: int = 0,
                    once: bool = False) -> typing.Tuple[int, ...]:
+        _log.debug("Input: %r, %r, %r, %r", num_dice, sides, reroll, once)
         lowest = min(1 if once else reroll+1, sides)
+        _log.debug("lowest == %r", lowest)
         _pool = [self._randint(lowest, sides) for _ in range(num_dice)]
         _pool.sort()
         if not once:
             return tuple(_pool)
+        _log.debug("Before reroll once: _pool == %r", _pool)
         for ind, val in enumerate(_pool):
             if val > reroll:
                 break
             _pool[ind] = self._randint(lowest, sides)
+        _log.debug("After reroll once and no sort: _pool == %r", _pool)
         _pool.sort()
         return tuple(_pool)
