24274c17f6199bbd0f051e1989b27a2122497d7a
1im <mcgahajr@ornl.gov>
2020-02-10 12:38:56 -0500

    Add testing for storing ability modifiers

--- test/test_ability_scores.py
+++ test/test_ability_scores.py
@@ -14,3 +14,9 @@ def test_ability():
     assert hasattr(sample_ability, '_base_score')
     assert sample_ability._base_score == ability_scores._Ability._default_score
     assert sample_ability() == 10
+    assert hasattr(sample_ability, '_mod_permanent')
+    assert hasattr(sample_ability, '_mod_temporary')
+    assert hasattr(sample_ability, '_mod_override')
+    assert sample_ability._mod_permanent == list()
+    assert sample_ability._mod_temporary == list()
+    assert sample_ability._mod_override == list()
