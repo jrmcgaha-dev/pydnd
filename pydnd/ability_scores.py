28b8f0a754a0d4e1c6bd48474d4b0183c0132720
1im <mcgahajr@ornl.gov>
2020-02-10 12:46:12 -0500

    Add loading of permanent modifiers on initialization

--- pydnd/ability_scores.py
+++ pydnd/ability_scores.py
@@ -5,14 +5,14 @@ class _Ability:
 
     _default_score = 10
 
-    def __init__(self, score: int = None):
+    def __init__(self, score: int = None, **permanent_modifiers):
         if score is None:
             score = self._default_score
         self._base_score = score
-        self._mod_permanent = dict()
+        self._mod_permanent = dict(**permanent_modifiers)
         self._mod_temporary = dict()
         self._mod_override = dict()
 
     def __call__(self) -> int:
         # TODO: Change to score property after creation of method
-        return self._base_score
+        return self._base_score + sum(self._mod_permanent.values())
