b4d98c9c171e83f713bb315bc692da6ea17afd41
1im <mcgahajr@ornl.gov>
2020-02-10 12:12:31 -0500

    Create basic test for _Ability class existence, creation, and callability

--- test/test_ability_scores.py
+++ test/test_ability_scores.py
@@ -3,3 +3,12 @@ from pydnd import ability_scores
 
 def test_imports():
     assert hasattr(ability_scores, 'typing')
+
+
+def test_ability():
+    assert hasattr(ability_scores, '_Ability')
+    assert callable(ability_scores._Ability)
+    assert hasattr(ability_scores._Ability, '_default_score')
+    assert ability_scores._Ability._default_score == 10
+    sample_ability = ability_scores._Ability()
+    assert sample_ability() == 10
