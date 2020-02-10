43d8d7ed29165dfb675efaf87643e29af8d88344
1im <mcgahajr@ornl.gov>
2020-02-10 12:17:54 -0500

    Create basics for _Ability class

--- pydnd/ability_scores.py
+++ pydnd/ability_scores.py
@@ -1,1 +1,15 @@
 import typing
+
+
+class _Ability:
+
+    _default_score = 10
+
+    def __init__(self, score: int = None):
+        if score is None:
+            score = self._default_score
+        self._base_score = score
+
+    def __call__(self) -> int:
+        # TODO: Change to score property after creation of method
+        return self._base_score
