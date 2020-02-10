ee9397fb2753592062bfbbe09d1771331be0bb2f
1im <mcgahajr@ornl.gov>
2020-02-10 12:15:33 -0500

    Add testing for base score and ability naming

--- test/test_ability_scores.py
+++ test/test_ability_scores.py
@@ -11,4 +11,8 @@ def test_ability():
     assert hasattr(ability_scores._Ability, '_default_score')
     assert ability_scores._Ability._default_score == 10
     sample_ability = ability_scores._Ability()
+    assert hasattr(sample_ability, '_base_score')
+    assert sample_ability._base_score == ability_scores._Ability._default_score
+    assert hasattr(sample_ability, 'name')
+    assert sample_ability.name == ''
     assert sample_ability() == 10
