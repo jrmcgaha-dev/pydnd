13eb1eabda688564cef3b179207c654ac0608797
1im <mcgahajr@ornl.gov>
2020-02-14 15:33:51 -0500

    Add test for class attribute for converting challenge rating to experience

--- test/test_monster.py
+++ test/test_monster.py
@@ -12,6 +12,7 @@ def test_monster():
     assert hasattr(monster, 'Monster')
     assert callable(monster.Monster)
     assert monster.Creature in monster.Monster.__bases__
+    assert hasattr(monster.Monster, '_challenge_to_experience')
     sample_monster = monster.Monster()
     assert hasattr(sample_monster, 'alignment')
     assert hasattr(sample_monster, 'challenge_rating')
