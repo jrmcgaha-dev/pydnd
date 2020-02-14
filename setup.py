424acfa216c7a18c6c5c75d848a7550c510bba11
1im <mcgahajr@ornl.gov>
2020-02-14 14:05:42 -0500

    Update python requirement to specify >=3.8

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='pydnd',
       version='0.0',
-      packages=find_namespace_packages())
+      packages=find_namespace_packages(),
+      python_requires=">=3.8", )
