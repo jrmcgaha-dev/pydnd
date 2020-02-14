19d2f3170f166be44f1985fa3ee5d4e75e04aa3a
1im <mcgahajr@ornl.gov>
2020-02-14 15:56:28 -0500

    Add loading of cr_to_xp table

--- pydnd/monster.py
+++ pydnd/monster.py
@@ -8,6 +8,12 @@ from pydnd.dice_bag import Roller
 
 
 class Monster(Creature):
+    _challenge_to_experience = json.load(
+        resource_stream(
+            "pydnd",
+            "cr_to_xp.json"
+        )
+    )
 
     def __init__(self):
         self.challenge_rating: int = 0
