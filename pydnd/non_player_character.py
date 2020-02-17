05ba078ae543cdce674197e0b6a30481a685eb9b
1im <mcgahajr@ornl.gov>
2020-02-17 13:25:05 -0500

    Add documentation to NonPlayerCharacter

--- pydnd/non_player_character.py
+++ pydnd/non_player_character.py
@@ -6,4 +6,15 @@ from pydnd.dice_bag import Roller
 
 
 class NonPlayerCharacter(_Character, Monster):
+    """NonPlayerCharacter is a sub-class of _Character and then Monster.
+    Non-player characters (or NPC's) are characters built by the Dungeon
+    Master and thus require the tools of both characters and monsters to
+    properly encapsulate all of their potential features.
+
+    Notes
+    -----
+    This class currently has no unique properties and serves as a placeholder
+    for the intended structure of later developments in pydnd.
+
+    """
     pass
