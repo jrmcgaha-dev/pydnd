c17471ad05ff18a3e6ab09345188bf838b8dd135
1im <mcgahajr@ornl.gov>
2020-02-17 10:19:15 -0500

    Create basic test for existence of main function

--- test/test_main.py
+++ test/test_main.py
@@ -6,3 +6,8 @@ def test_imports():
     assert hasattr(__main__, 'logging')
     assert hasattr(__main__, 'sys')
     assert hasattr(__main__, 'dice_bag')
+
+
+def test_main():
+    assert hasattr(__main__, 'main')
+    assert callable(__main__.main)
