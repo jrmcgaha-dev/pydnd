2888c167990ed007d890cabf57d38f716a89cfb4
1im <mcgahajr@ornl.gov>
2020-02-17 10:05:29 -0500

    Add to import test

--- test/test_main.py
+++ test/test_main.py
@@ -4,4 +4,5 @@ from pydnd import __main__
 def test_imports():
     assert hasattr(__main__, 'argparse')
     assert hasattr(__main__, 'logging')
+    assert hasattr(__main__, 'sys')
     assert hasattr(__main__, 'dice_bag')
