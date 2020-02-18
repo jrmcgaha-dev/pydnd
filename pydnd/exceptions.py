b91c57576ded082ed3a8a330306b0eac31cb1652
1im <mcgahajr@ornl.gov>
2020-02-18 13:03:01 -0500

    Create ParseError

--- pydnd/exceptions.py
+++ pydnd/exceptions.py
@@ -1,2 +1,8 @@
 class PydndException(Exception):
+    """Base exception class for pydnd"""
+    pass
+
+
+class ParseError(PydndException):
+    """Raised when an input value fails to parse correctly"""
     pass
