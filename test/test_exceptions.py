6e09c4964b20341f6c9323e506ade2be9cfae768
1im <mcgahajr@ornl.gov>
2020-02-18 12:09:27 -0500

    Create test for ParseError exception

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -8,3 +8,14 @@ def test_root_exception():
         raise exceptions.PydndException
     except exceptions.PydndException:
         assert True
+
+
+def test_parsing_exception():
+    assert hasattr(exceptions, 'ParseError')
+    assert exceptions.PydndException in exceptions.ParseError.__bases__
+    try:
+        raise exceptions.ParseError
+    except exceptions.PydndException:
+        assert True
+    except exceptions.ParseError:
+        assert True
