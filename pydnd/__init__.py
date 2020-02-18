6b0ca9c091f8c4b1c95b94bc6539ccf435c5d2e5
1im <mcgahajr@ornl.gov>
2020-02-18 11:05:57 -0500

    Create unified logger in init

--- pydnd/__init__.py
+++ pydnd/__init__.py
@@ -1,7 +1,17 @@
 """pydnd serves as a manager for various Dungeons and Dragons entities
 with rolling and interaction integrated with storage and customization.
 """
+import logging
+
 from pydnd import ability_scores, creature, monster
 from pydnd.dice_bag import Roller
+
+logging.basicConfig(
+    format='%(name)s.%(module)s [%(asctime)s] [%(levelname)s]: %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.INFO,
+)
+
+_log = logging.getLogger('pydnd')
 
 roll = Roller().roll
