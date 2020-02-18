3adfe3fbec3b98dac823241ab806cf5f31515f79
1im <mcgahajr@ornl.gov>
2020-02-18 12:05:37 -0500

    Create test for root exception

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -1,1 +1,10 @@
 from pydnd import exceptions
+
+
+def test_root_exception():
+    assert hasattr(exceptions, 'PydndException')
+    assert Exception in exceptions.PydndException.__bases__
+    try:
+        raise exceptions.PydndException
+    except exceptions.PydndException:
+        assert True
