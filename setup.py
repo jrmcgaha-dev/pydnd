df42e462e11131fb77795ad4743f4e00ffba8ba8
1im <mcgahajr@ornl.gov>
2020-02-18 10:44:59 -0500

    Add more metadata to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='pydnd',
     version='0.1.1',
+    description='Flexible Dungeons and Dragons entity manager and helper',
+    author='Jesse McGaha',
+    url='https://github.com/jrmcgaha-dev/pydnd',
     packages=find_namespace_packages(),
     python_requires='>=3.8',
     package_data={
