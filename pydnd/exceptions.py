a8446cf90d8e7556171b6b8f81e5c8463234f295
1im <mcgahajr@ornl.gov>
2020-02-18 13:05:58 -0500

    Create AbilityError

--- pydnd/exceptions.py
+++ pydnd/exceptions.py
@@ -6,3 +6,9 @@ class PydndException(Exception):
 class ParseError(PydndException):
     """Raised when an input value fails to parse correctly"""
     pass
+
+
+class AbilityError(PydndException):
+    """Raised when an input value is not an Ability or cannot be converted
+    to an Ability"""
+    pass
