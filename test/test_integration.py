f486d60d172b56ba2e364de954b56794413fc5d7
1im <mcgahajr@ornl.gov>
2020-02-18 13:54:21 -0500

    Create test that goes through typical use of pydnd

---
+++ test/test_integration.py
@@ -0,0 +1,34 @@
+import pydnd
+
+
+def test_rolling():
+    assert 1 <= pydnd.roll('1d20') <= 20
+    assert 5 <= pydnd.roll('1d20+4') <= 24
+
+
+def test_monster():
+    goblin = pydnd.monster.Monster()
+    goblin.name = 'Bill'
+    assert goblin.name == 'Bill'
+    goblin.size = 'small'
+    assert goblin.size == 'small'
+    assert goblin.size_multiplier == 1
+    goblin.type = 'humanoid (goblin)'
+    assert goblin.type == 'humanoid (goblin)'
+    goblin.attributes['str'] = 8
+    goblin.attributes['dex'] = 14
+    goblin.attributes['con'] = 10
+    goblin.attributes['int'] = 10
+    goblin.attributes['wis'] = 8
+    goblin.attributes['cha'] = 8
+    assert goblin.attributes['str']() == (8, -1)
+    assert 0 <= goblin.attributes.roll('str') <= 19
+    goblin.hit_dice_number = 2
+    assert goblin.hit_dice_number == 2
+    goblin.challenge_rating = 1/4
+    assert goblin.avg_hp == 7
+    assert goblin.proficiency == 2
+    assert goblin.experience == 50
+    goblin.add_language('Common')
+    goblin.add_language('Goblin')
+    assert goblin.languages == 'Common, Goblin'
